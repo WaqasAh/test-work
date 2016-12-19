fff8d0b04a9903e9622fd95d07f8f938725276c4
Waqas Ahmed <waqasahmed@Waqass-MacBook-Pro.local>
2016-12-19 15:08:14 +0000

    added files and creating folder to test

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,2 +1,14 @@
-include apache
-include nginx
+#node 'puppet4agent.localdomain' {
+#hiera_include(classes)
+#}
+
+#node 'puppet4agent.localdomain' {
+#    hiera_include('classes')
+#  }
+
+#node 'puppet4agent2.localdomain' {
+#        include nginx
+#  }
+
+
+hiera_include(classes)
